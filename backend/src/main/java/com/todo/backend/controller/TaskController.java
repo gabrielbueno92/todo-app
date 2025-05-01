b4403d63d032ce1d4ac1c45889a14f5dce821e3f
Gabriel Bueno <gabi_bueno92@hotmail.com>
2025-05-01 19:43:53 +1000

    Model, repository and controller created

---
+++ backend/src/main/java/com/todo/backend/controller/TaskController.java
@@ -0,0 +1,47 @@
+package com.todo.backend.controller;
+
+import com.todo.backend.model.Task;
+import com.todo.backend.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Task> getTaskById(@PathVariable Long id){
+        return taskRepository.findById(id);
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskRepository.save(task);
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable Long id, @RequestBody Task taskDetails) {
+        Task task = taskRepository.findById(id).orElseThrow();
+        task.setTitle(taskDetails.getTitle());
+        task.setDescription(taskDetails.getDescription());
+        task.setCompleted(taskDetails.isCompleted());
+        return taskRepository.save(task);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id) {
+        taskRepository.deleteById(id);
+    }
+
+}
